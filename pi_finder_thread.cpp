7942e180265edf07f094fd04f6db82c205a8c192
amoo-milad <30766645+amoo-milad@users.noreply.github.com>
2017-09-25 03:52:55 +0330

    Fixed. now it's perfect

--- pi_finder_thread.cpp
+++ pi_finder_thread.cpp
@@ -1,72 +1,43 @@
-// pi_finder_thread.cpp	   ->   estimates the Pi number.
+// pi_finder_thread.cpp :  Estimates the Pi number.
 
 #include "stdafx.h"
 #include <conio.h>
 #include <windows.h>
-#include <time.h>
 
-double random()
-{
-	//srand(time(NULL)); // no good when their time is the same
-	return (double)rand() / (double)((unsigned)RAND_MAX + 1);
-	// needs #include <stdlib.h>  or #include <windows.h>
-}
-
-//#define BATCH_SIZE 1000000000	// takes time, about 60 secs.
 #define BATCH_SIZE	 10000000
+
+double random();
+DWORD WINAPI MyThreadFunc(LPVOID lpParameter);
 
 struct ThreadParams 
 {
-	long int inCircle;
+	long long inCircle;
+	int seed;
 };
-
-DWORD WINAPI MyThreadFunc(LPVOID lpParameter)
-{
-	ThreadParams* tp = (ThreadParams*) lpParameter; 
-
-	long int inCircleCount = 0;                        
-
-	for (int i = 0; i < BATCH_SIZE / 4 ; i++) 
-	{
-		double x = random();
-		double y = random();
-		if (x*x + y*y < 1)
-			inCircleCount++;
-	}
-	
-	tp->inCircle = inCircleCount;
-	printf(" number of inCircles: %ld\n", inCircleCount);
-	printf("thread finished\n");
-	return 0;
-}
 
 int main()
 {
 	ThreadParams tp1, tp2, tp3, tp4;
+	//the initializing of "tpn.inCircle = 0;" are not necessary because they're gonna be written (and not read) in the Func. 
 
-	tp1.inCircle = 0;
-	tp2.inCircle = 0;
-	tp3.inCircle = 0;
-	tp4.inCircle = 0;
+	tp1.seed = 1;
+	tp2.seed = 2;
+	tp3.seed = 3;
+	tp4.seed = 4;
 
-	CreateThread(NULL, 0, &MyThreadFunc, &tp1, 0, NULL);
-	CreateThread(NULL, 0, &MyThreadFunc, &tp2, 0, NULL);
-	CreateThread(NULL, 0, &MyThreadFunc, &tp3, 0, NULL);
-	CreateThread(NULL, 0, &MyThreadFunc, &tp4, 0, NULL);
+	DWORD thread_id1 = 0;
+	HANDLE h1 = CreateThread(NULL, 0, &MyThreadFunc, &tp1, 0, &thread_id1);
+	HANDLE h2 = CreateThread(NULL, 0, &MyThreadFunc, &tp2, 0, NULL);
+	HANDLE h3 = CreateThread(NULL, 0, &MyThreadFunc, &tp3, 0, NULL);
+	HANDLE h4 = CreateThread(NULL, 0, &MyThreadFunc, &tp4, 0, NULL);
 
-	_getch(); 
-	//// instead of '_getch();' we can also use a bigger time_consuming process, the joker:
-	//long int joker = 0;
-	//for (int i = 0; i < BATCH_SIZE ; i++) 
-	//{
-	//	double x = random();
-	//	double y = random();
-	//	if (x*x + y*y < 1)
-	//		joker++;
-	//}
-	//printf("\nthe Joker is %ld\n", joker); // indicates that probably the other four processes has already finished!
+	//WaitForSingleObject(h1, INFINITE);
+	//WaitForSingleObject(h2, INFINITE); // and like this for all the 4 threads...
 
-	long long int sum = (long long int) tp1.inCircle + tp2.inCircle + tp3.inCircle + tp4.inCircle;
+	HANDLE handles[] = {h1, h2, h3, h4};
+	WaitForMultipleObjects(4, handles, TRUE, INFINITE);
+
+	long long sum = ((long long) tp1.inCircle) + tp2.inCircle + tp3.inCircle + tp4.inCircle;
 	printf("\nthe sum of all inCircles is %lld\n", sum);
 
 	double estimateForPi = 4 * ((double) sum / BATCH_SIZE);
@@ -74,4 +45,32 @@ int main()
 
 	_getch();
 	return 0;           
+}
+
+double random()
+{
+	return (double)rand() / (double)((unsigned)RAND_MAX + 1);
+	// rand functions need either #include <stdlib.h>  or #include <windows.h>
+}
+
+DWORD WINAPI MyThreadFunc(LPVOID lpParameter)
+{
+	ThreadParams* tp = (ThreadParams*) lpParameter; 
+
+	long long inCircleCount = 0;                        
+
+	srand(tp->seed);
+
+	for (int i = 0; i < BATCH_SIZE / 4 ; i++) 
+	{
+		double x = random();
+		double y = random();
+		if (x*x + y*y <= 1.0)
+			inCircleCount++;
+	}
+	
+	tp->inCircle = inCircleCount;
+	printf(" number of inCircles: %lld for seed %d\n", inCircleCount, tp->seed);
+	printf("thread finished\n");
+	return 0;
 }
